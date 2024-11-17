5671c6917c61fc3e366ea220d963988534991405
rahul-verma-30 <rahul.verma@webninjaz.com>
2024-11-17 20:25:50 +0530

    2 commit

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,9 +1,13 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const page = () => {
     return (
         <div>
             Dashboard
+            <Button>
+                Login
+            </Button>
 
         </div>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,23 @@
+"use client"
+
+import { Button } from '@/components/ui/button'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
-const page = () => {
+const Page = () => {
+  const router = useRouter()
+
   return (
     <div>
       <main>
-        Hello
+        <h1>Hello</h1>
+        <div className='flex space-x-4'>
+          <Button onClick={() => router.push('/signup')}>Sign Up</Button>
+          <Button onClick={() => router.push('/login')}>Login</Button>
+        </div>
       </main>
-
     </div>
   )
 }
 
-export default page
+export default Page
